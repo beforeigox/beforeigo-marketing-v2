e00ad6fbb5232a49393f4cd2e49f8d1f95ef341c
Kasey Lycar <kaseylycar@Kaseys-MBP.home.local>
2026-04-08 03:13:52 -0700

    Add sitemap as serverless function

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,15 +1,20 @@
-<?xml version="1.0" encoding="UTF-8"?>
+module.exports = (req, res) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://beforeigo.app/</loc>
-    <lastmod>2026-04-06</lastmod>
+    <lastmod>2026-04-08</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://beforeigo.app/checkout</loc>
-    <lastmod>2026-04-06</lastmod>
+    <lastmod>2026-04-08</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(sitemap);
+};
